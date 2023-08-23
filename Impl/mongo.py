c2f229c6368301188d4a52d41d42a423d44a61af
Maxim <s6584094@stud.uni-frankfurt.de>
2023-08-23 22:44:56 +0200

    mongo connection enabled

---
+++ Impl/mongo.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+import urllib.parse
+
+def get_mongoconnec():
+    user = urllib.parse.quote_plus("Maxim")
+    pw = urllib.parse.quote_plus("Mongoserver1")
+    client = MongoClient('mongodb://%s:%s@10.77.77.46' % (user, pw), 27017)
+    return client
+
+def get_mongodb(client):
+    db = client.bundestag
+    return db
+
+def get_mongocoll(db):
+    coll = db.prots
+    return coll
+
