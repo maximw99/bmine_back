f20450ffd1d0cf07a66677adb30dfbe204e0a2e2
Maxim <s6584094@stud.uni-frankfurt.de>
2023-09-11 13:38:00 +0200

    check

--- src/api/api.py
+++ src/api/api.py
@@ -3,25 +3,73 @@ sys.path.append("src")
 from databank import mongoconnec
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+import json
+from objects import *
 app = Flask(__name__)
 CORS(app)
-client = mongoconnec.get_mongoconnec()
-db = mongoconnec.get_mongodb(client)
-coll = mongoconnec.get_mongocoll(db)
 
-@app.route("/get-speaker", methods=["Get"])
-def home():
-    curr =  coll.find({"_id" : "19-3"})
-    response = curr[0]["daytopics"][0]["speeches"][0]["speaker"]
-    
+@app.route("/get-oneprot/<prot_id>", methods=["Get"])
+def one_prot(prot_id):
+    client = mongoconnec.get_mongoconnec()
+    db = mongoconnec.get_mongodb(client)
+    coll = mongoconnec.get_mongocoll(db)
+    curr =  coll.find({"_id" : prot_id})
+    response = curr[0]
+    client.close()
 
-    return response
+    return jsonify(response)
 
+
+@app.route("/get-allprot", methods=["Get"])
+def all_prot():
+    client = mongoconnec.get_mongoconnec()
+    db = mongoconnec.get_mongodb(client)
+    coll = mongoconnec.get_mongocoll(db)
+
+    counter = 0
+    prots = []
+    curr =  coll.find({}).allow_disk_use(True)
+    for doc in curr:
+        print(curr)
+        print(counter)
+        counter += 1
+        prots.append(doc)
+    client.close()
+    prots_json = {"prots" : prots}
+
+    return jsonify(prots_json)
+
+
+@app.route("/get-speakerov", methods=["Get"])
+def one_speaker():
+    client = mongoconnec.get_mongoconnec()
+    db = mongoconnec.get_mongodb(client)
+    coll = mongoconnec.get_mongocoll(db)
+
+    speaker = speaker()
+    curr =  coll.find({}).allow_disk_use(True)
+    for doc in curr:
+        prot: prot = doc
+        print(prot.id)
+    client.close()
+
+    return None
+
+
+def test():
+    client = mongoconnec.get_mongoconnec()
+    db = mongoconnec.get_mongodb(client)
+    coll = mongoconnec.get_mongocoll(db)
+
+    speaker_obj: speaker = speaker.Speaker()
+    curr =  coll.find({}).allow_disk_use(True)
+    for doc in curr:
+        prot: prot = doc
+        print(prot["_id"])
+    client.close()
+
+
+test()
 
 def start_api():
     app.run(debug=True)
-
-print(type(coll.find_one()))
-
-print(type(coll.find({})))
